18b96f8dac2d2505f3d620bf59253417bf1fb8b4
BuscaProfesores Chile <buscaprofesores@users.noreply.github.com>
2018-01-07 11:26:00 -0300

    Update city-tour-data.js

--- city-tour-data.js
+++ city-tour-data.js
@@ -1,21 +1,21 @@
 var treeData = [
   {
-    "name": "Pensar es Clave, Federico Sánchez",
+    "name": "City Tour",
     "parent": "null",
     "children": [
-      {     "name": "Julio",
+      {     "name": "2009",
             "_children": [
              { "name":   "jueves 20 - Joe Vascncellos",   "url": "https://www.youtube.com/v/jKKM-KMyQvo"      },      
              { "name":    "Mentes Brillantes 1 - Ramón Latorre"  ,"url":   "https://www.youtube.com/v/voogNkB14gM"    }
            ]
       },
-      {     "name": "Septiembre",
+      {     "name": "2010",
             "_children": [
              { "name":   "viernes 1 - Fabrizio Copano",   "url": "https://www.youtube.com/v/kDlTMDMYo-U"      },      
              { "name":    "Mentes Brillantes 1 - Ramón Latorre"  ,"url":   "https://www.youtube.com/v/voogNkB14gM"    }
            ]
       },
-    {       "name": "Octubre",
+    {       "name": "2011",
             "_children": [
              { "name":   "Mentes Brillantes 2 - Jorge Allende",        "url": "https://www.youtube.com/v/joXU0YgKu4Q"  },
              {"name":    "Mentes Brillantes 2 - Ricardo Uauy"     ,"url":   "https://www.youtube.com/v/oNgIp0ifo0s"     }
