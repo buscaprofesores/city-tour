0a5e10389cbcd080a9637eb15f19118d222f4168
BuscaProfesores Chile <buscaprofesores@users.noreply.github.com>
2018-01-07 14:12:20 -0300

    Update city-tour-data.js

--- city-tour-data.js
+++ city-tour-data.js
@@ -3,22 +3,58 @@ var treeData = [
     "name": "City Tour",
     "parent": "null",
     "children": [
-      {     "name": "2009",
+    {     "name": "temporada 1: 2009",
             "_children": [
-             { "name":   "jueves 20 - Joe Vascncellos",   "url": "https://www.youtube.com/v/jKKM-KMyQvo"      },      
-             { "name":    "Mentes Brillantes 1 - Ramón Latorre"  ,"url":   "https://www.youtube.com/v/voogNkB14gM"    }
+             { "name":   "Capítulo 1",   "url": "https://www.youtube.com/v/zxrM84pCNwM"      },      
+             { "name":    "Capitulo "  ,"url": "https://www.youtube.com/"    }
            ]
-      },
-      {     "name": "2010",
+    },
+    {     "name": "2010",
             "_children": [
-             { "name":   "viernes 1 - Fabrizio Copano",   "url": "https://www.youtube.com/v/kDlTMDMYo-U"      },      
-             { "name":    "Mentes Brillantes 1 - Ramón Latorre"  ,"url":   "https://www.youtube.com/v/voogNkB14gM"    }
+             { "name":   "Capitulo",   "url": "https://www.youtube.com/"      },      
+             { "name":    "Capitulo"  ,"url":   "https://www.youtube.com/"    }
            ]
-      },
-    {       "name": "2011",
+    },
+    {     "name": "2011",
             "_children": [
-             { "name":   "Mentes Brillantes 2 - Jorge Allende",        "url": "https://www.youtube.com/v/joXU0YgKu4Q"  },
-             {"name":    "Mentes Brillantes 2 - Ricardo Uauy"     ,"url":   "https://www.youtube.com/v/oNgIp0ifo0s"     }
+             { "name":   "Capitulo",   "url": "https://www.youtube.com/"      },      
+             { "name":    "Capitulo"  ,"url":   "https://www.youtube.com/"    }
+           ]
+    },	
+    {     "name": "2012",
+            "_children": [
+             { "name":   "Capitulo",   "url": "https://www.youtube.com/"      },      
+             { "name":    "Capitulo"  ,"url":   "https://www.youtube.com/"    }
+           ]
+    },	
+    {     "name": "2013",
+            "_children": [
+             { "name":   "Capítulo 2",   "url": "https://www.youtube.com/v/if_QC9l5gcs"      },      
+             { "name":    "Capítulo 28"  ,"url":   "https://www.youtube.com/v/ZHQHh0ptWjg"    }
+           ]
+    },	
+    {     "name": "2014",
+            "_children": [
+             { "name":   "Capítulo 24",   "url": "https://www.youtube.com/v/2nPWvumT7sk"      },      
+             { "name":    "Capítulo"  ,"url":   "https://www.youtube.com/"    }
+           ]
+    },	
+    {     "name": "2015",
+            "_children": [
+             { "name":   "Capitulo",   "url": "https://www.youtube.com/"      },      
+             { "name":    "Capitulo"  ,"url":   "https://www.youtube.com/"    }
+           ]
+    },	
+    {     "name": "2016",
+            "_children": [
+             { "name":   "Capitulo",   "url": "https://www.youtube.com/"      },      
+             { "name":    "Capitulo"  ,"url":   "https://www.youtube.com/"    }
+           ]
+    },	
+    {       "name": "2017",
+            "_children": [
+             { "name":   "Capitulo",   "url": "https://www.youtube.com/"      },      
+             { "name":    "Capitulo"  ,"url":   "https://www.youtube.com/"    }
             ]
      }
   
